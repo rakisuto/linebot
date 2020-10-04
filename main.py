da546eb6e6cc281030beb576b64b5b67f1b71727
rakisuto <tama5.bo6bo6@gmail.com>
2020-10-04 16:42:33 +0900

    test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 app = Flask(__name__)
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["    "]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
