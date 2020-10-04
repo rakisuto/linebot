ceeab637217fb95aa5044e9eed4d4f518da55fcb
rakisuto <tama5.bo6bo6@gmail.com>
2020-10-04 19:27:50 +0900

    test

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
+
 )
 from linebot.exceptions import (
+
     InvalidSignatureError
+
 )
 from linebot.models import (
+
     MessageEvent, TextMessage, TextSendMessage,
+
 )
 import os
-
 app = Flask(__name__)
+LINE_CHANNEL_ACCESS_TOKEN = os.environ[“LINE_CHANNEL_ACCESS_TOKEN”]
+LINE_CHANNEL_SECRET = os.environ[“LINE_CHANNEL_SECRET”]
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+@app.route(“/callback”, methods=[‘POST’])
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-@app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    signature = request.headers[‘X-Line-Signature’]
+
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    app.logger.info(“Request body: ” + body)
+
     try:
+
         handler.handle(body, signature)
+
     except InvalidSignatureError:
+
         abort(400)
 
-    return 'OK'
-
+    return ‘OK’
 
 @handler.add(MessageEvent, message=TextMessage)
+
+#オウム返し用のメッセージイベント
+
 def handle_message(event):
+
     line_bot_api.reply_message(
+
         event.reply_token,
-        TextSendMessage(text=event.message.text))
 
+        TextSendMessage(text=event.message.text)
 
-if __name__ == "__main__":
-#app.run()
-    port = int(os.getenv("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    )
+
+if __name__ == “__main__”:
+
+    port = int(os.getenv(“PORT”))
+
+    app.run(host=”0.0.0.0″, port=port)
